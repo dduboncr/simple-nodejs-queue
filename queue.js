64eabe5c29cd3d3476848c5773ce9d101bff96ad
Diego Dubon <diego.dubon@proximitycr.com>
2020-09-09 19:18:45 -0600

    create queue class

---
+++ queue.js
@@ -0,0 +1,42 @@
+
+const events = require('events');
+
+class AsyncQueue extends events.EventEmitter {
+    constructor(){
+
+        const eventEmitter = new events.EventEmitter();
+        this.queueEvents = eventEmitter
+        this.queue = []
+
+        const listner1 = function listner1(item) {
+            console.log('enqueue',{item});
+         }
+        eventEmitter.addListener('enqueue',listner1);
+    }
+
+    start(){
+
+    }
+    pause(){ 
+    }
+    
+    enqueue(item){
+        this.queueEvents.on('enqueue', listner2(item));
+        this.queue.push(item);
+    }
+
+    peek(){
+       if(this.queue.length === 0){
+           return null
+       }else{
+        return this.queue[0];
+       }
+      
+    }
+
+    print(){
+        return this.queue
+    }
+}
+
+module.exports = AsyncQueue
